a722582891ee3833104fe023db708727924850c0
Evan Culver <eculver@uber.com>
2017-11-03 08:05:24 -0700

    Add more examples to timedelta

--- cmd/timedelta/main.go
+++ cmd/timedelta/main.go
@@ -7,12 +7,19 @@ import (
 )
 
 func main() {
-	delta, err := time.ParseDuration("4320h") // 6 months = 6 * 30 * 24
+	duration := "4320h"                                                     // 6 months = 6 * 30 * 24
+	expiredTime := time.Date(2017, time.January, 10, 12, 0, 0, 2, time.UTC) // expired
+	notExpiredTime := time.Now().UTC()                                      // not expired
+
+	delta, err := time.ParseDuration(duration)
 	if err != nil {
-		log.Fatalf("Could not parse duration: %v", err)
+		log.Fatalf("Could not parse duration (%s): %v", duration, err)
 	}
-	// delta := time.Duration(4320) * time.Hour
+
 	expiresOn := time.Now().Add(delta)
 	fmt.Printf("expiration: %s\n", expiresOn)
 	fmt.Printf("time until expiration: %s\n", time.Until(expiresOn))
+
+	fmt.Printf("is %s more than %s old?: %t\n", expiredTime, duration, time.Since(expiredTime) > delta)
+	fmt.Printf("is %s more than %s old?: %t\n", notExpiredTime, duration, time.Since(notExpiredTime) > delta)
 }
