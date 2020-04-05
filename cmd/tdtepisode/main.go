6888a20319c5cbc5e3411deb56e0597de4eefc0e
Evan Culver <e@eculver.io>
2020-04-05 14:39:54 -0700

    braindump of components and workflow

---
+++ cmd/tdtepisode/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const (
+	introPath      = "/Users/eculver/sync/src/tdt/episode_notes/intro.html"
+	guestTplName   = "guest.tpl.txt"
+	noguestTplName = "noguest.tpl.txt"
+)
+
+func main() {
+	// try to download notes?
+	// try to download media?
+
+	// upload media to CDN
+
+	// generate episode HTML
+
+	// upload episode HTML to web server as draft
+	// uploadHTML(html, destination, status, slug)
+
+	// email Daniele
+	fmt.Println("a")
+}
